9b58c424aebef9e3d68c8007997c6081ea251b2a
HarshWebd <upsign443@gmail.com>
2025-10-29 22:16:43 +0530

    fix3

--- Server/index.js
+++ Server/index.js
@@ -13,23 +13,31 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-// const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // CORS only in dev
-app.use(cors({
-  origin: 'https://student-digital-portfolio.onrender.com',
-  credentials: true,
-}));
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }));
+}
 
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api', router);
 
-app.use(express.static(path.join(__dirname, "/Client/dist")));
-app.get("*", (_, res)=> {
-  res.sendFile(path.resolve(__dirname, "/Client", "dist", "index.html"));
-})
+// ✅ Serve frontend in production
+if (process.env.NODE_ENV === 'production') {
+  const distPath = path.join(__dirname, '../Client/dist');
+  app.use(express.static(distPath));
+
+  // 👇 Catch-all route for React
+  app.use((req, res) => {
+    res.sendFile(path.join(distPath, 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
