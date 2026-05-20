6fa413dfc36cafe1e116bcc23e4a78d4f6bca90d
HarshWebd <upsign443@gmail.com>
2026-05-21 01:41:37 +0530

    adding server url

--- Client/src/utils/axiosInstance.utils.js
+++ Client/src/utils/axiosInstance.utils.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  // baseURL: "https://student-digital-portfolio.onrender.com/api" || 'http://localhost:5000/api',
+  // baseURL: 'http://localhost:5000/api',
+  baseURL: "https://student-digital-portfolio.onrender.com/api" || 'http://localhost:5000/api',
   withCredentials: true,
 });
 
